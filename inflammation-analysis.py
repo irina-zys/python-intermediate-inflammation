ba21108e7086a34fa603c6eaeba9be9ee8986287
irina-zys <yishuo_zhang@outlook.com>
2023-05-16 10:39:44 +0100

    Code style fixes.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -21,7 +21,11 @@ def main(args):
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        # Using hanging indent with the, closing '}' aligned with the start of the multiline contruct
+        view_data = {
+            'average': models.daily_mean(inflammation_data),
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
 
